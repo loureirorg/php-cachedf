cbac68bea5a5fdff60ded52dbdb69893a6553360
Daniel Loureiro <loureirorg@gmail.com>
2013-04-01 06:25:47 -0300

    benchmark at example

--- example.php
+++ example.php
@@ -1,14 +1,33 @@
 <?php
 include_once "cachedf.php";
 
-function soma($a, $b)
+function sum($a, $b)
 {
-	if (cachedf()) return cachedf_val(); // ALWAYS ADD AT FIRST LINE OF FUNCTION
-	
-	return	$a + $b;
+    if (cachedf()) return cachedf_val(); // ALWAYS ADD THIS AT FIRST LINE OF FUNCTION
+    
+    // simulate expensive code
+    sleep(2);
+    
+    return  $a + $b;
 }
 
 $a = rand()%1000000;
 $b = rand()%1000000;
-echo soma(soma($a, $b), soma(1,soma($a,$b)));
+
+// benchmark: we'll call 2 times - first without cache, then with cache:
+$time_start = microtime(true);
+$r1 = sum(sum($a, $b), sum(1, sum($a, $b)));
+$time_end_1 = microtime(true);
+$r2 = sum(sum($a, $b), sum(1, sum($a, $b)));
+$time_end_2 = microtime(true);
+
+// benchmark result:
+echo ($r1 == $r2)? '$r1 and $r2 are equals<br />': 'something has got wrong! $r1 and $r2 aren\'t equals<br />';
+echo "1st call (without cache): ". ($time_end_1 - $time_start) ." seconds<br />";
+echo "2nd call (with cache): ". ($time_end_2 - $time_end_1) ." seconds<br />";
+
+// output:
+// $r1 and $r2 are equals
+// 1st call (without cache): 6.0010 seconds
+// 2nd call (with cache): 0.0001 seconds
 ?>
